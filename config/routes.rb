bd9a893106af7722bf61c88d2d01182434b1316c
gir1ng <a08030582573a@gmail.com>
2020-05-14 10:53:10 +0900

    ログイン機能を作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   get "users/new"
-  resources :menus
-  # get "menus/new", to: "menus#new"
   root "mahizap#index"
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+  resources :menus
   resources :users
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
   def setup
-    @user = User.new(name: "test", email: "test@test.com", password: "test", password_confirmation: "test")
+    @user = User.new(name: "test", email: "test@test.com", password: "testtt", password_confirmation: "testtt")
   end
 
   test "should be valid" do
