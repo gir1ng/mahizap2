d3667d681bb5567f26ba5307d01d5a2fe1f5196c
gir1ng <a08030582573a@gmail.com>
2020-05-16 12:08:45 +0900

    menusコントローラーを削除

--- app/controllers/menus_controller.rb
+++
@@ -1,7 +0,0 @@
-class MenusController < ApplicationController
-  before_action :logged_in_user
-  before_action :correct_user
-
-  def new
-  end
-end
--- test/controllers/menus_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class MenusControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get new_menu_url
-    assert_response :success
-  end
-end
